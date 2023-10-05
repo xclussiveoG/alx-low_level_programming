edc2c4381eaeb0afeca3ef0f7f3b8a0d1c39a10b
Xclussiveog <ikireziremymagnom@gmail.com>
2023-10-05 11:58:14 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main -Entry original me
+ * Return: 0 xclussive
+ **/
+
+int main(void)
+{
+	/*our project is included in c puts*/
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
