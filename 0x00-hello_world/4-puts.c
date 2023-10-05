d4c37de3d9b602b9fcf0f9dcf5bd271ea258beb7
Xclussiveog <ikireziremymagnom@gmail.com>
2023-10-05 12:19:21 -0700

    my message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * Main -Entry original me
+ *
  * Return: 0 xclussive
- **/
-
+ */
 int main(void)
 {
-	/*our project is included in c puts*/
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
