95b5d66350e040d604d501ff20e6685cce8a71ce
Xclussiveog <ikireziremymagnom@gmail.com>
2023-10-06 11:47:39 -0700

    101 print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
+ * main - prints all possible different combinations of three digits
  *
  * Return: Almost 0 (done)
  */
@@ -10,27 +9,40 @@
 int main(void)
 
 {
+	int n, m, l;
 
-	char ch = 'a';
-
-	while (ch <= 'z')
-
+	for (n = 48; n < 58; n++)
 	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
 
-		if (ch != 'e' && ch != 'q')
+				{
+					putchar(n);
 
-	{
+					putchar(m);
 
-		putchar(ch);
+					putchar(l);
+
+					if (n != 55 || m != 56)
+
+					{
+						putchar(',');
+
+						putchar(' ');
+					}
+
+				}
+
+			}
+
+		}
 
 	}
 
-		ch++;
-
-	}
-
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
-
 }
