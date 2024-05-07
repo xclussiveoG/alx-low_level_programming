eb7dfdc8824f705a479592977b8655fa5f4b437c
Xclussiveog <ikireziremymagnom@gmail.com>
2024-05-07 09:16:12 -0700

    100 jmp2

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,81 +1,44 @@
+#include "search_algos.h"
 #include <math.h>
 
 /**
- * perform_linear_search - searches for a value in a sorted array of integers
- * using the Linear search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * @prev: previous index
- * @jump: jump index
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
- * Return: index where value is located, or -1 on failure
- */
-int perform_linear_search(int *array, size_t size, int value, size_t prev,
-						  size_t jump)
-{
-	while (array[prev] < value && prev < size)
-	{
-		print_value_checked(array, prev);
-		if (prev == MIN(jump, size))
-			return (-1);
-
-		prev++;
-	}
-
-	if (array[prev] == value)
-	{
-		print_value_checked(array, prev);
-		return (prev);
-	}
-
-	return (-1);
-}
-
-/**
- * jump_search - searches for a value in a sorted array of integers
- * using the Jump search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- *
- * Return: index where value is located, or -1 on failure
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t prev, jump;
+	int index, m, k, prev;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	prev = 0;
-	jump = sqrt(size);
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-	print_value_checked(array, prev);
-	while (array[MIN(jump, size) - 1] < value)
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		prev = jump;
-		jump += sqrt(size);
-
-		/* oops! we went overboard, the value does not exist in the array */
-		if (jump >= size)
-		{
-			print_value_checked(array, prev);
-			break;
-		}
-
-		print_value_checked(array, prev);
-
-		/*
-		 * we found the target value either at the start or endof block size.
-		 * Perform linear search to find the exact index
-		 */
-		if (array[prev] == value || array[jump] == value)
-			break;
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, jump);
-
-	return (perform_linear_search(array, size, value, prev, jump));
+	return (-1);
 }
-
